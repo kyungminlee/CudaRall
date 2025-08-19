539b698acdfc4a7d50088d97a885e55ce29bc5f4
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2025-08-18 22:48:28 -0700

    Commit. Update.

--- src/CudaRall.hh
+++ src/CudaRall.hh
@@ -1,5 +1,8 @@
 #pragma once
 #include <cstddef>
+#include <initializer_list>
+#include <iostream>
+
 #include "CudaMacros.hh"
 
 template <typename T, std::size_t D>
@@ -10,14 +13,11 @@ public:
   using size_type = std::size_t;
   using difference_type = std::ptrdiff_t;
 
-  template <typename U>
-  friend class CudaRall;
-
-  constexpr CudaRall() = default;
-  constexpr CudaRall(CudaRall const &) = default;
-  constexpr CudaRall(CudaRall &&) = default;
-  constexpr CudaRall & operator=(CudaRall const &) = default;
-  constexpr CudaRall & operator=(CudaRall &&) = default;
+  constexpr CudaRall() noexcept = default;
+  constexpr CudaRall(CudaRall const &) noexcept = default;
+  constexpr CudaRall(CudaRall &&) noexcept = default;
+  constexpr CudaRall & operator=(CudaRall const &) noexcept = default;
+  constexpr CudaRall & operator=(CudaRall &&) noexcept = default;
 
   CUDA_HOST CUDA_DEVICE
   explicit constexpr CudaRall(value_type const & value) noexcept : _value(value) {
@@ -27,9 +27,34 @@ public:
   }
 
   CUDA_HOST CUDA_DEVICE
+  constexpr CudaRall & operator=(value_type const & value) noexcept {
+    _value = value;
+    for (size_type i = 0; i < dimension; ++i) {
+      _gradient[i] = value_type();
+    }
+    return *this;
+  }
+
+  CUDA_HOST CUDA_DEVICE
   constexpr CudaRall(value_type const & value, const value_type gradient[dimension]) noexcept : _value(value) {
     for (size_type i = 0; i < dimension; ++i) {
       _gradient[i] = gradient[i];
+    }
+  }
+
+  CUDA_HOST CUDA_DEVICE
+  CudaRall(value_type const & value, std::initializer_list<value_type> gradients) noexcept : _value(value) {
+    size_type i = 0;
+    for (auto const & grad : gradients) {
+      if (i < dimension) {
+        _gradient[i] = grad;
+        ++i;
+      } else {
+        break;
+      }
+    }
+    for (; i < dimension; ++i) {
+      _gradient[i] = value_type();
     }
   }
 
@@ -62,13 +87,19 @@ public:
     return !(*this == other);
   }
 
+  //
   CUDA_HOST CUDA_DEVICE
-  constexpr bool operator<(CudaRall const & other) const noexcept {
-    if (_value != other._value) return _value < other._value;
+  friend constexpr CudaRall operator+(CudaRall const & x) noexcept {
+    return x;
+  }
+
+  CUDA_HOST CUDA_DEVICE
+  friend constexpr CudaRall operator-(CudaRall const & x) noexcept {
+    CudaRall out(-x._value);
     for (size_type i = 0; i < dimension; ++i) {
-      if (_gradient[i] != other._gradient[i]) return _gradient[i] < other._gradient[i];
+      out._gradient[i] = -x._gradient[i];
     }
-    return false;
+    return out;
   }
 
   CUDA_HOST CUDA_DEVICE
@@ -109,6 +140,8 @@ public:
     return *this;
   }
 
+  // bin op
+
   CUDA_HOST CUDA_DEVICE
   friend constexpr CudaRall operator+(CudaRall const & x, CudaRall const & y) noexcept {
     CudaRall out(x._value + y._value);
@@ -146,15 +179,6 @@ public:
   }
 
   CUDA_HOST CUDA_DEVICE
-  friend constexpr CudaRall operator-(CudaRall const & x) noexcept {
-    CudaRall out(-x._value);
-    for (size_type i = 0; i < dimension; ++i) {
-      out._gradient[i] = -x._gradient[i];
-    }
-    return out;
-  }
-
-  CUDA_HOST CUDA_DEVICE
   friend constexpr CudaRall operator+(CudaRall const & x, value_type const & y) noexcept {
     CudaRall out(x._value + y);
     for (size_type i = 0; i < dimension; ++i) {
@@ -171,7 +195,7 @@ public:
     }
     return out;
   }
-
+  
   CUDA_HOST CUDA_DEVICE
   friend constexpr CudaRall operator-(CudaRall const & x, value_type const & y) noexcept {
     CudaRall out(x._value - y);
@@ -190,21 +214,88 @@ public:
     return out;
   }
 
+  CUDA_HOST CUDA_DEVICE
+  friend constexpr CudaRall operator*(CudaRall const & x, value_type const & y) noexcept {
+    CudaRall out(x._value * y);
+    for (size_type i = 0; i < dimension; ++i) {
+      out._gradient[i] = x._gradient[i] * y;
+    }
+    return out;
+  }
+
+  CUDA_HOST CUDA_DEVICE
+  friend constexpr CudaRall operator*(value_type const & x, CudaRall const & y) noexcept {
+    CudaRall out(x * y._value);
+    for (size_type i = 0; i < dimension; ++i) {
+      out._gradient[i] = x * y._gradient[i];
+    }
+    return out;
+  }
+
+  CUDA_HOST CUDA_DEVICE
+  friend constexpr CudaRall operator/(CudaRall const & x, value_type const & y) noexcept {
+    CudaRall out(x._value / y);
+    for (size_type i = 0; i < dimension; ++i) {
+      out._gradient[i] = x._gradient[i] / y;
+    }
+    return out;
+  }
+
+  CUDA_HOST CUDA_DEVICE
+  friend constexpr CudaRall operator/(value_type const & x, CudaRall const & y) noexcept {
+    CudaRall out(x / y._value);
+    for (size_type i = 0; i < dimension; ++i) {
+      out._gradient[i] = -x * y._gradient[i] / (y._value * y._value);
+    }
+    return out;
+  }
+
+  constexpr value_type const & value() const noexcept { return _value; }
+  constexpr value_type const & gradient(size_type i) const noexcept { return _gradient[i]; }
+  constexpr value_type const * gradient() const noexcept { return _gradient; }
+
+  friend std::ostream & operator<<(std::ostream & os, CudaRall const & rall) {
+    os << "{ Value: " << rall._value << ", Gradient: [";
+    char const * sep = "";
+    for (std::size_t i = 0; i < dimension; ++i) {
+      os << sep << rall._gradient[i];
+      sep = ", ";
+    }
+    os << "]}";
+    return os;
+  }
+
 protected:
   value_type _value = value_type();
   value_type _gradient[D] = {0};
 };
 
 template <typename T, std::size_t D>
-CudaRall<T, D> pow(CudaRall<T, D> const & x, std::size_t n) {
-  CudaRall<T, D> result(1);
-  CudaRall<T, D> base = x;
-  while (n > 0) {
-    if (n & 0x1) {
-      result *= base;
+CUDA_HOST CUDA_DEVICE
+constexpr CudaRall<T, D> pow(CudaRall<T, D> const & x, int n) noexcept {
+  if (n < 0) {
+    return 1 / pow(x, -n);
+  } else {
+    CudaRall<T, D> result(1);
+    CudaRall<T, D> base = x;
+    while (n > 0) {
+      if (n & 0x1) {
+        result *= base;
+      }
+      base *= base;
+      n >>= 1;
     }
-    base *= base;
-    n >>= 1;
+    return result;    
   }
-  return result;
+}
+
+template <typename T, std::size_t D>
+CUDA_HOST CUDA_DEVICE
+constexpr CudaRall<T, D> sqrt(CudaRall<T, D> const & x) noexcept {
+  CudaRall<T, D> out(sqrt(x.value()));
+  T const deriv_factor = T(0.5) / out.value();
+  for (std::size_t i = 0; i < D; ++i) {
+    out._gradient[i] = deriv_factor * x.gradient(i);
+  }
+  return out;
 }
--- test/test.cc
+++ test/test.cc
@@ -1,6 +1,34 @@
 #include "CudaRall.hh"
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
 
 int main()
 {
-  
+  // test CudaRall class
+
+  CudaRall<double, 3> a(2.0, {3.0, 4.0, 5.0});
+  CudaRall<double, 3> b(3.0, {6.0, 7.0, 8.0});
+  CudaRall<double, 3> c = a + b;
+  CudaRall<double, 3> d = a - b;
+  CudaRall<double, 3> e = a * b;
+  CudaRall<double, 3> f = a / b;
+
+  std::cout << "a = " << a << std::endl;
+  std::cout << "b = " << b << std::endl;
+  std::cout << "c = " << c << std::endl;
+  std::cout << "d = " << d << std::endl;
+  std::cout << "e = " << e << std::endl;
+  std::cout << "f = " << f << std::endl;
+  assert((c == CudaRall<double, 3>(5.0, {9.0, 11.0, 13.0})));
+  assert((d == CudaRall<double, 3>(-1.0, {-3.0, -3.0, -3.0})));
+  assert((e == CudaRall<double, 3>(6.0, {21.0, 26.0, 31.0})));
+
+
+
+  std::cout << a * a + pow(a, 5) << std::endl;
+
+
+  return 0;
 }
